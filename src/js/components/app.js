2a3de707b916dff3384fd07a6f50034054e1f0ba
Marcos Sampaio and Roger Almeida <just-dev+msampaio+ralmeida@tyro.com>
2018-06-11 20:12:51 +1000

    SHowing a sample phaser.io 3 canva

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import Header from './header';
 
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <h1>Hello World from React with Bootstrap</h1>
+        <Header />
+        <main>
+          <div id="game-container"></div>
+        </main>
       </div>
     )
   }
