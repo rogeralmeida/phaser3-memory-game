67086ee1fcd78f71db6d9db13fdf777a4fdb2f50
Marcos Sampaio and Roger Almeida <just-dev+msampaio+ralmeida@tyro.com>
2018-06-11 19:04:56 +1000

    Using react to render the hello world

--- src/js/main/index.js
+++ src/js/main/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+var rootElement = document.createElement('div');
+rootElement.id = "root";
+document.body.appendChild(rootElement);
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,13 @@ module.exports = {
 	module: {
 		rules: [
 			{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+			{
 				test: /\.css$/,
 
 				use: [
