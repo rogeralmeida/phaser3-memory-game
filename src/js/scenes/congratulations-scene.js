9ddd34fa0b828c2370911b7e6c6f5a9cdd644d13
Roger Almeida <ralmeida@tyro.com>
2019-05-15 07:14:21 +1000

    congratulation scene is working with the large screen

--- src/js/scenes/congratulations-scene.js
+++ src/js/scenes/congratulations-scene.js
@@ -1,7 +1,7 @@
 import Phaser from 'phaser'
 
 const BORGENS_BURLESQUE_FONT_NAME = 'borgerns-burlesque'
-const BACKGROUND_IMAGE_KEY = 'background-blackjack'
+const BACKGROUND_IMAGE_KEY = 'background'
 const BUTTON_1_KEY = 'button_1'
 
 class CongratulationsScene extends Phaser.Scene {
@@ -17,13 +17,14 @@ class CongratulationsScene extends Phaser.Scene {
   }
 
   create(data){
-    this.add.image(0, 0, BACKGROUND_IMAGE_KEY)
-    var text = this.add.bitmapText(60, 200, BORGENS_BURLESQUE_FONT_NAME, "Congratulations", 94);
-    let newGameButton = this.add.image(400, 400, BUTTON_1_KEY);
+    let backgroundImage = this.add.image(0, 0, BACKGROUND_IMAGE_KEY)
+    backgroundImage.setScale(2, 2);
+    var text = this.add.bitmapText(300, 200, BORGENS_BURLESQUE_FONT_NAME, "Congratulations", 94);
+    let newGameButton = this.add.image(620, 400, BUTTON_1_KEY);
     newGameButton.setScale(0.45);
     newGameButton.centerX = newGameButton.width / 2;
     newGameButton.centery = newGameButton.heigth / 2;
-    this.add.bitmapText(300, 375, BORGENS_BURLESQUE_FONT_NAME, "Play Again", 48);
+    this.add.bitmapText(510, 375, BORGENS_BURLESQUE_FONT_NAME, "Play Again", 48);
     newGameButton.setInteractive();
     newGameButton.on('click', this._newGameButtonClicked, this);
     this.input.on('gameobjectup', (pointer, gameObject) => {
